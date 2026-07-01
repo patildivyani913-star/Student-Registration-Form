2045749e0a2c9877db613fc320ff70763626b986
Divyani patil <patildivyani913@gmail.com>
2026-07-01 17:35:49 +0530

    main added

---
+++ main.js
@@ -0,0 +1,25 @@
+function calculate(operator){
+    let num1  = Number(document.getElementById("num1").value);
+    let num2  = Number(document.getElementById("num2").value);
+
+    let result;
+
+    if(operator === '+'){
+        result = num1 + num2;
+    }
+    else if(operator === '-'){
+        result = num1 - num2;
+    }
+    else if(operator === '*'){
+        result = num1 * num2;
+    }
+    else if(operator === '/'){
+        result = num1 / num2;
+    }
+    document.getElementById("result").innerHTML="Result:"+result;
+    console.log("Result =",result);
+
+
+
+
+}
