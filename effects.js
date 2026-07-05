b5dabf4b701c6abe23c1404081981daa3ba8e6b9
Divyani patil <patildivyani913@gmail.com>
2026-07-05 17:11:11 +0530

    effects added

---
+++ effects.js
@@ -0,0 +1,69 @@
+function changeTextColor(){
+
+    document.getElementById("heading").style.color = "red";
+
+}
+
+// Change Background Color
+function changeBackground(){
+
+    document.body.style.backgroundColor = "skyblue";
+
+}
+
+// Change Font Size
+function changeFontSize(){
+
+    document.getElementById("heading").style.fontSize = "40px";
+
+}
+
+// Change Width
+function changeWidth(){
+
+    document.getElementById("box").style.width = "300px";
+
+}
+
+// Change Height
+function changeHeight(){
+
+    document.getElementById("box").style.height = "200px";
+    document.getElementById("box").style.lineHeight = "200px";
+
+}
+
+// Hide the Box
+function hideBox(){
+
+    document.getElementById("box").style.display = "none";
+
+}
+
+// Show the Box
+function showBox(){
+
+    document.getElementById("box").style.display = "block";
+
+}
+
+// Change Border
+function changeBorder(){
+
+    document.getElementById("box").style.border = "3px solid green";
+
+}
+
+// Change Border Radius
+function changeBorderRadius(){
+
+    document.getElementById("box").style.borderRadius = "15px";
+
+}
+
+// Change Box Shadow
+function changeShadow(){
+
+    document.getElementById("box").style.boxShadow = "5px 5px 15px gray";
+
+}
